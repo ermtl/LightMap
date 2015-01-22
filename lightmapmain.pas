b4a2a54abd2f13d2978d93d99f94d5c90e1c28ee
ermtl <erv@mailpeers.net>
2015-01-21 21:06:27 -0500

    Initial

--- lightmapmain.pas
+++ lightmapmain.pas
@@ -1,5 +1,27 @@
 unit LightMapMain;
 
+{
+  This software is Copyright 1988-2014 by Eric Vinter and is
+  distributed under the terms of the GNU General Public License.
+
+  This program is free software; you can redistribute it
+  and/or modify it under the terms of the GNU General Public
+  License as published by the Free Software Foundation;
+  either version 2 of the License, or any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see < http://www.gnu.org/licenses/ >.
+
+
+  Contact: https://github.com/ermtl/LightMap
+
+}
+
 {$mode objfpc}{$H+}
 
 interface
